2efb9fde7b870cb4597ccf68d688498a98039b63
gcoriaml <gaston.coria@mercadolibre.com>
2020-08-27 09:43:23 -0300

    remove puts used for test purposes, replace split for chars to improve readability

--- anagram.rb
+++ anagram.rb
@@ -1,8 +1,8 @@
 def is_anagram?(word, another_word)
-  puts word.split("").sort.join("") == another_word.split("").sort.join("")
+  word.chars.sort.join("") == another_word.chars.sort.join("")
 end
 
 is_anagram?("asd","dsa")
 is_anagram?("asd","daaa")
 is_anagram?("pepe","eppe")
-is_anagram?("asd","dsa")
+
